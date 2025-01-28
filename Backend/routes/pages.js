73e5faa1fe70205fd1b0ef8ff9ee5b143a64b5bd
LAPTOP-DQ8F023M\frank <Frank>
2025-01-28 15:39:38 +0000

    Adding logics for login -Backend

--- Backend/routes/pages.js
+++ Backend/routes/pages.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Get route .... for normal practice we can remove or update based on our requirements
 router.get("/",(req , res) => {
-    res.send("<h1>Welcome to our Team Project -Vinyl Valut</h1>")
+    res.send("<h1>Welcome to our Team Project -Vinyl Valut , Ecommerce music selling website</h1>")
    })
 
    //Used to export all the  functionality  from this file so it can be used in another file.
---
+++ Backend/routes/signup.js
@@ -0,0 +1,22 @@
+const express = require (express);
+const router = express.Router();
+
+//Importing the mock data from data folder
+const users = require('./data/mockData');
+
+// For generating unique user_id ,uuid package
+const { v4: uuidv4 } = require('uuid'); 
+
+// For date formatting
+const moment = require('moment'); 
+const { route } = require('./login');
+
+//Handling a POST request for signup route
+router.post("/signup",(req , res)=>{
+    const{ userName, email, password, phone_num, role } = req.body; 
+})
+
+/***Validation***/
+  if (!userName || !email || !password || !phone_num || !role) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
