1fb28578e3e35e5b39506ec871eea4500654d553
NiloufarSadeghpour <240283621@aston.ac.uk>
2025-01-30 17:23:17 +0000

    Resolved merge conflicts and added new components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // Import App instead of LandingPage
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Render App instead of LandingPage */}
   </React.StrictMode>
 );
 
