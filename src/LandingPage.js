6ce28fc15c9fc8490297573cd3c504a1c4c5240f
minankk <minankahai@gmail.com>
2025-02-02 21:50:15 +0000

    new js and css files on FE for components to contain global navbar and footer as issues were arrising with login display on react, many other FE changes for readability and smoothness, xolours etc will be updated

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,30 +1,9 @@
 import React from "react";
-import "./LandingPage.css"; // Assuming your styles are in LandingPage.css
+import "./LandingPage.css";
 
-function LandingPage() { //sth sth even more sth
+function LandingPage() {
   return (
-    <div>
-      <header>
-        <div className="header-left">
-          <div className="logo">Logo</div>
-          <nav className="navbar">
-            <ul>
-              <li><a href="#">Homepage</a></li>
-              <li><a href="#">New in Store</a></li>
-              <li><a href="#">Best Sellers</a></li>
-              <li><a href="#">Sale</a></li>
-              <li><a href="#">Browse</a></li>
-            </ul>
-          </nav>
-        </div>
-        <div className="search-cart">
-          <input type="text" placeholder="Search here" />
-          <button>🔍</button>
-          <button>🛒</button>
-          <button>👤</button>
-        </div>
-      </header>
-
+    <div className="landing-container">
       <section className="banner">
         <div className="banner-content">
           {/* Placeholder for banner content */}
@@ -43,26 +22,6 @@ function LandingPage() { //sth sth even more sth
           <div className="item">Item 6</div>
         </div>
       </section>
-
-      <footer>
-        <div className="footer-links">
-          <div>
-            <h4>Company</h4>
-            <p><a href="#">About Us</a></p>
-            <p><a href="#">Stakeholders</a></p>
-          </div>
-          <div>
-            <h4>Customer Service</h4>
-            <p><a href="#">Contact Us</a></p>
-            <p><a href="#">My Account</a></p>
-          </div>
-        </div>
-        <div className="social-icons">
-          <button>🐦</button>
-          <button>📸</button>
-          <button>📧</button>
-        </div>
-      </footer>
     </div>
   );
 }
