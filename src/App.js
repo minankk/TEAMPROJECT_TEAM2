6e11ff4e893b43c15ed435f24c4d1008b85516cb
minankk <minankahai@gmail.com>
2025-02-02 00:39:30 +0000

    fixed issue with login page nmot displaying in react, fixed missing login route in App.js, fixed incorrect form action in LoginPage.js, fixed navigation link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
-import ProductsPage from './ProductsPage'; //hahahah
+import ProductsPage from './ProductsPage';
 import LoginPage from './LoginPage';
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} /> {/* Landing page route */}
-        <Route path="/products" element={<ProductsPage />} /> {/* Products page route */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/login" element={<LoginPage />} /> {/* Added Login Route */}
       </Routes>
     </Router>
   );
