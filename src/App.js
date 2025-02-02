966c02e6dbd8101dddcd5db7fa1d9ce08476ac2c
minankk <minankahai@gmail.com>
2025-02-02 21:50:15 +0000

    new js and css files on FE for components to contain global navbar and footer as issues were arrising with login display on react, many other FE changes for readability and smoothness, xolours etc will be updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import LandingPage from './LandingPage';
 import ProductsPage from './ProductsPage';
 import LoginPage from './LoginPage';
@@ -7,11 +9,13 @@ import LoginPage from './LoginPage';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/login" element={<LoginPage />} /> {/* Added Login Route */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
