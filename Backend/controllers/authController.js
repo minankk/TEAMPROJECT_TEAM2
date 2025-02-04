538c4ddc94c4e72bc8660a6067870619ca06fa31
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-02-04 11:43:25 +0000

    Added DB query with the login backend route

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,38 @@
 const users = require('../data/mockData');
+const db = require('../db')
 
 exports.login = (req, res) => {
+    const { username, password } = req.body;
+
+    // Query the database for the user
+    const query = 'SELECT * FROM users WHERE user_name = ? AND password = ?';
+    
+    db.query(query, [username, password], (err, results) => {
+        if (err) {
+            console.error('Database query error:', err);
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+
+        if (results.length > 0) {
+          const user = results[0];
+            // User found, set session variables
+            req.session.loggedIn = true;
+            req.session.username = user.user_name;
+            req.session.user_id = user.user_id;
+            req.session.role = user.role;
+
+            return res.status(200).json({ 
+                message: 'Login successful', 
+                user: { username: user.user_name }
+            });
+        } else {
+            console.log("Invalid login attempt");
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+    });
+};
+
+/*exports.login = (req, res) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
     if (user) {
@@ -16,7 +48,7 @@ exports.login = (req, res) => {
     // Return error response
       return res.status(400).json({ message: 'Invalid credentials' });
     }
-  };
+  };*/
 
 exports.logout = (req, res) => {
     // Destroy the session
