6564b8d00b8f1fbdc195e4a7ab1b5db54fb4ed41
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-02-05 13:34:43 +0000

    Added db

--- Backend/db.js
+++ Backend/db.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql2")
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',               // Database host
-  user: 'root',                    // Database username
-  password: 'teamProject@01',      // Database password
-  database: 'vinyldatabase',       // Database name
+  host:     '127.0.0.1'  ,          //'cs2410-web01pvm.aston.ac.uk' ,    //'134.151.36.17'        //     '127.0.0.1',               // Database host
+  user:   'root',               //'cs4team2' ,                                   // 'root',                    // Database username
+  password:  'teamProject@01',            //'zCiGwGdHiEL4gOP' ,                                   //'teamProject@01',      // Database password
+  database:   'vinyldatabase' ,      //'cs4team2_db',                                   // 'vinyldatabase',       // Database name
   port: 3306,                      // Database port (default MySQL port)
 });
 
