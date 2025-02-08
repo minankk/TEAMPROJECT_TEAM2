d744e6dff3506f6465eb539e3cae54f4a75e2c30
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-02-08 15:47:56 +0000

    Added node modules to git ignore and added session middleware in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -32,8 +32,15 @@ const app = express();
 // Middleware to parse incoming request bodies as JSON and for CORS => frontend if running on different host
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();  // Allow request to proceed
+});
 
-app.use(cors());
+
+app.use(cors({
+  credentials: true 
+}));
 
 // Session middleware to manage user login state
 app.use(session({
