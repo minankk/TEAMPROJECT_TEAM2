c3a5aeb0889cff6e44782d3d7731c9640220aeac
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-02-13 20:14:36 +0000

    Modified some folders

--- Backend/controllers/checkSession.js
+++ Backend/controllers/checkSession.js
@@ -1,6 +1,6 @@
 const db = require('../db')
 
-// This route checks if the user is logged in
+// This logic checks if the user is logged in
 exports.checkSession = (req, res) => {
     if (req.session.loggedIn) {
         return res.status(200).json({ loggedIn: true });
