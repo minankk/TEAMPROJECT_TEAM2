57c8361afa5637b21721c3b1cacfe5dc92c2602c
HirokiKurano <240361251@aston.ac.uk>
2025-03-03 15:28:11 +0000

    Added import React ,{ useState, useEffect } from "react"; . Ashmin has been checked

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+
+import React ,{ useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import './CartPage.css';
 import OriginalSoundtrackBarbietheAlbum from './assets/OriginalSoundtrackBarbietheAlbum.webp';
