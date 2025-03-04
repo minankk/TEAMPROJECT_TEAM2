7d43d8a1d3945ce473d52ebfd9aef7f4d9048db7
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-04 19:07:05 +0000

    Added a backend logic for the newest addition

--- Backend/controllers/pageController.js
+++ Backend/controllers/pageController.js
@@ -28,3 +28,32 @@ exports.getArtists = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch artists" });
   });
 };
+
+//to get newest addition
+exports.getNewestAdditions = async (req, res) => {
+  try {
+    const [rows] = await db.execute(`
+      SELECT 
+        p.product_id, 
+        p.name AS album_name, 
+        a.name AS artist_name, 
+        g.name AS genre,
+        p.release_date, 
+        p.price, 
+        p.cover_image_url
+      FROM products p
+      JOIN artists a ON p.artist_id = a.artist_id
+      JOIN genres g ON p.genre_id = g.genre_id
+      WHERE p.newest_addition = 1
+    `);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "No newest additions found." });
+    }
+
+    res.status(200).json({ products: rows });
+  } catch (error) {
+    console.error('Error fetching newest additions:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
