1b598c31b8a63564a59f76292de5bd65f8b69815
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-04 18:23:13 +0000

    Added the artist backend logic for the artist page

--- Backend/controllers/pageController.js
+++ Backend/controllers/pageController.js
@@ -14,3 +14,17 @@ exports.dashboard = (req, res) => {
   });
 };
 
+//to view the artist
+exports.getArtists = async (req, res) => {
+  db.execute(`
+    SELECT artist_id, name, bio, profile_image_url
+    FROM artists
+  `)
+  .then(([results]) => {
+    res.status(200).json(results);
+  })
+  .catch((err) => {
+    console.error("Error fetching artists:", err);
+    res.status(500).json({ error: "Failed to fetch artists" });
+  });
+};
