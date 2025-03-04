1c7f4201a9ac44a3c22df9f2ac276cbbef697303
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-04 17:59:10 +0000

    Modified the page controller

--- Backend/controllers/pageController.js
+++ Backend/controllers/pageController.js
@@ -3,16 +3,6 @@ const formatDate = require('../helpers/dateFormatter');
 const formatCurrency = require('../helpers/currencyFormatter')
 const stringSimilarity = require('string-similarity');
 
-exports.landingPage = async (req, res) => {
-  if (req.session.loggedIn) {
-      // Redirect to dashboard if already logged in
-      return res.redirect('/dashboard'); 
-  }
-
-  // Otherwise, show the landing page
-  res.status(200).json({ message: 'Welcome to the landing page' }); 
-};
-
 exports.dashboard = (req, res) => {
   if (!req.session.loggedIn) {
     return res.status(401).json({ message: "Please log in" });
