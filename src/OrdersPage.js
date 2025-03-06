be57d4281aeb78c1197c538f4c1cf4f9feb91595
NiloufarSadeghpour <240283621@aston.ac.uk>
2025-03-06 16:11:36 +0000

    added an orders page

---
+++ src/OrdersPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './OrdersPage.css';
+
+const OrdersPage = () => {
+  return (
+    <div>
+      <h2>Orders</h2>
+      <p>This is the Orders page.</p>
+    </div>
+  );
+};
+
+export default OrdersPage;
