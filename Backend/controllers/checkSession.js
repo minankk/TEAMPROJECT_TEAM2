d02f8961df41fcd8d3f9919c269bc218ec193fd9
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-06 14:33:21 +0000

    Modified the view dashboard

--- Backend/controllers/checkSession.js
+++ Backend/controllers/checkSession.js
@@ -1,5 +1,17 @@
-const db = require('../db')
+const jwt = require('jsonwebtoken');
 
 exports.checkSession = (req, res) => {
-    res.status(200).json({ loggedIn: true, user: req.user });
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        return res.status(401).json({ loggedIn: false, message: 'Access Denied: No token provided' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({ loggedIn: false, message: 'Invalid or expired token' });
+        }
+
+        res.status(200).json({ loggedIn: true, user });
+    });
 };
