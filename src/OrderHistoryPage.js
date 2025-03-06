21a659e211ee4dad77133f33070ab8010e896884
NiloufarSadeghpour <240283621@aston.ac.uk>
2025-03-06 16:11:03 +0000

    added an order history page

---
+++ src/OrderHistoryPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './OrderHistoryPage.css'
+
+const OrderHistoryPage = () => {
+  return (
+    <div>
+      <h2>Order History</h2>
+      <p>This is the Order History page.</p>
+    </div>
+  );
+};
+
+export default OrderHistoryPage;
