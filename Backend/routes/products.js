282ce2b10a49e04df81ad67f78e081cbe99216ae
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-12 23:09:40 +0000

    resolving

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -3,32 +3,31 @@ const router = express.Router();
 const productsController = require('../controllers/productController');
 const searchController = require('../controllers/searchController');
 
-
 // GET - for all products
 router.get('/', productsController.getAllProducts);
 
-//GET - Filters by decade
+// GET - Filters by decade
 router.get('/decade/:decade', productsController.filterByDecade);
 
-//get - Filters by price
+// GET - Filters by price
 router.get('/price/:price', productsController.filterByPrice);
 
-//get - Filters by best-sellers
+// GET - Filters by best-sellers
 router.get('/bestsellers', productsController.filterBestSellers);
 
-//get - Filters for onsale
+// GET - Filters for onsale
 router.get('/onsale', productsController.filterOnSale);
 
-//get - Filters for onsale 
+// GET - Filters by artist
 router.get('/artist/:artist', productsController.filterByArtist);
 
-// get - Filter products by genre
-router.get('/genre/:genre', productsController.filterByGenre);
+// GET - Filter products by genre (with optional artist filter)
+router.get('/genre/:genre', productsController.filterByGenre); // This is the route where both genre and artist can be filtered
 
-// to get the search products
+// GET - to get the search products
 router.get('/search', searchController.searchProducts);
 
-//to get the products by their ID(Whishlist)
-router.get('/products/:productId', productsController.getProductById); 
+// GET - to get the products by their ID (Whishlist)
+router.get('/products/:productId', productsController.getProductById);
 
 module.exports = router;
