8431d637954a0a0a5abb6a1c49bd33b9452df714
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-13 16:21:49 +0000

    Modified the cart and wishlist routes

--- Backend/routes/myCart.js
+++ Backend/routes/myCart.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
-const authenticateJWT = require('../middlewares/jwtAuthMiddleware');
 
-router.post('/add', authenticateJWT, cartController.addToCart);
-router.get('/:user_id', authenticateJWT, cartController.getCartItems);
+router.post('/add', cartController.addToCart);
+router.get('/:user_id',  cartController.getCartItems);
 router.delete('/remove/:cart_id', cartController.removeFromCart);
-router.post('/place-order', authenticateJWT, cartController.placeOrder);
-router.get("/cart-count/:user_id", authenticateJWT, cartController.cartCount);
+router.post('/place-order',  cartController.placeOrder);
+router.get("/cart-count/:user_id", cartController.cartCount);
 
 module.exports = router;
