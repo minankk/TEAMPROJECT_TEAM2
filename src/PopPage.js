3ad9bc915f11902f28b4be02b1af51539ecc2426
Rakshit18198 <240247151@aston.ac.uk>
2025-03-20 17:39:57 +0000

    pop genre page.js

---
+++ src/PopPage.js
@@ -0,0 +1,49 @@
+// PopPage.js
+import React, { useState, useEffect } from 'react';
+import './PopPage.css'; // Ensure you have this CSS file
+import { useNavigate } from 'react-router-dom';
+
+const PopPage = ({ handleAddToCart }) => {
+  const [popAlbums, setPopAlbums] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch('http://localhost:5001/genres/pop')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPopAlbums(data.products);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error('Error fetching data:', err);
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading Pop albums...</p>;
+  if (error) return <p>Error loading Pop albums: {error.message}</p>;
+
+  return (
+    <div className="pop-page-container">
+      <h1>Pop Albums</h1>
+      <div className="pop-albums-grid">
+        {popAlbums.map((album) => (
+          <div className="pop-album-card" key={album.product_id}>
+            <img src={`http://localhost:5001${album.cover_image_url}`} alt={album.product_name} className="pop-album-image" />
+            <h2 className="pop-album-name">{album.product_name}</h2>
+            <p className="pop-album-price">${album.price}</p>
+            <button className="add-to-cart-pop" onClick={() => handleAddToCart(album.product_id)}>
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopPage;
