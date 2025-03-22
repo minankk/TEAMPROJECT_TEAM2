6b0d05044a34d0537470b92ed9434c375a1e5729
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-03-22 13:40:28 +0000

    Added the BE logic to view all membership related list

--- Backend/controllers/adminMembershipController.js
+++ Backend/controllers/adminMembershipController.js
@@ -19,3 +19,17 @@ exports.cancelVIPMembership = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+// Get List of VIP Members
+exports.getVIPMembersList = async (req, res) => {
+    try {
+        const [vipMembers] = await db.execute(
+            "SELECT user_id, user_name, email FROM users WHERE membership_status = 'vip'"
+        );
+
+        res.status(200).json(vipMembers);
+    } catch (error) {
+        console.error("Error fetching VIP members:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
--- Backend/routes/adminRoutes/adminMembership.js
+++ Backend/routes/adminRoutes/adminMembership.js
@@ -5,5 +5,8 @@ const adminMembershipController = require("../../controllers/adminMembershipCont
 // Route to cancel VIP membership
 router.post("/cancel-vip", adminMembershipController.cancelVIPMembership);
 
+// Route to get VIP members list
+router.get("/vip-members",adminMembershipController.getVIPMembersList);
+
 
 module.exports = router;
