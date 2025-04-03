267aacfca43a00fcad52d33319e97e2a5f5df354
Rakshit18198 <240247151@aston.ac.uk>
2025-04-03 16:03:15 +0100

    genre page and Blogpage

---
+++ src/GenrePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LandingPage.css";
+import VinylGenres from "./VinylGenres";
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <VinylGenres />
+    </div>
+  );
+}
+
+export default LandingPage;
