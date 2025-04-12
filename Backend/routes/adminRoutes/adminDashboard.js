4789f7aa33dd88bf78f22ce50a4ba0a318b42f6e
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-04-12 20:11:50 +0100

    Fixed the product management

--- Backend/routes/adminRoutes/adminDashboard.js
+++ Backend/routes/adminRoutes/adminDashboard.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const adminDashboardController = require('../../controllers/adminDashboardController');
 
+// admin - dashboard -> overview
+router.get('/',  adminDashboardController.getDashboardData);
+
+/**
+ * Analytics section
+ */
+
 //report -sales
 router.get('/sales-report', adminDashboardController.getSalesReport);
 
@@ -11,6 +18,10 @@ router.get('/user-activity-report', adminDashboardController.getUserActivityRepo
 //report - product report
 router.get('/product-report', adminDashboardController.getProductReport);
 
+/**
+ * Order Management
+ */
+
 //report - order report
 router.get('/order-report', adminDashboardController.getOrderReport);
 
@@ -18,15 +29,21 @@ router.get('/order-report', adminDashboardController.getOrderReport);
 router.delete('/orders/cancel/:orderId', adminDashboardController.cancelOrder);
 
 
-// GET /admin/dashboard (Get dashboard data including reports and products)
-router.get('/',  adminDashboardController.getDashboardData);
+/**
+ * Product Management
+ */
+
+//view all products
 router.get('/products', adminDashboardController.getAllProducts);
 
+//add a product
+router.post('/add/products', adminDashboardController.addProduct);
 
-// Product Management Endpoints (within the dashboard)
-router.post('/products', adminDashboardController.addProduct);
-router.put('/products/:id',  adminDashboardController.updateProduct);
-router.delete('/products/:id', adminDashboardController.deleteProduct);
+//update a product
+router.put('/update/products/:id',  adminDashboardController.updateProduct);
+
+
+router.delete('/delete/products/:id', adminDashboardController.deleteProduct);
 
 
 
